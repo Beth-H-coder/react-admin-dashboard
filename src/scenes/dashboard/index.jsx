767e4e7df24cea6364c27e04ffee70bcde46147a
Beth-H-coder <77305766+Beth-H-coder@users.noreply.github.com>
2025-02-25 19:07:01 +0000

    add sidebar

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,5 +1,20 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const theme = useTheme();
+  const colours = tokens(theme.palette.mode);
+  return (
+    <Box>
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+      <Box></Box>
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,54 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColourModeContext, tokens } from "../../theme";
+import { InputBase } from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
 const Topbar = () => {
-  return <div>Topbar</div>;
+  const theme = useTheme();
+  const colours = tokens(theme.palette.mode);
+  const colourMode = useContext(ColourModeContext);
+  return (
+    <>
+      <Box display="flex" justifyContent="space-between" p={2}>
+        <Box
+          display="flex"
+          backgroundColor={colours.primary[400]}
+          borderRadius="3px"
+        >
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="search" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+
+        <Box display="flex">
+          <IconButton onClick={colourMode.toggleColourMode}>
+            {theme.palette.mode === "dark" ? (
+              <LightModeOutlinedIcon />
+            ) : (
+              <DarkModeOutlinedIcon />
+            )}
+          </IconButton>
+          <IconButton>
+            <NotificationsOutlinedIcon />
+          </IconButton>
+
+          <IconButton>
+            <SettingsOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <PersonOutlinedIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Topbar;
